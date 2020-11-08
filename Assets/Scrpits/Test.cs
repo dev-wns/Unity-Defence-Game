cf478aff2770f9b806145e02150d65df225ec3b9
wnsghks <ashi0@naver.com>
2020-11-08 11:49:07 +0900

    [깃 연동] 기본 브랜치 main -> master 변경 확인용 커밋

--- Assets/Scrpits/Test.cs
+++ Assets/Scrpits/Test.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<Transform>().Rotate( Time.deltaTime * 10.0f, 0.0f, 0.0f );
     }
 }
